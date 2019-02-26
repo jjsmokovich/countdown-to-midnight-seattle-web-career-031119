04c054744feb66e5221eaface4b647cf96748000
Jesse Smokovich <jessesmokovich@yahoo.com>
2019-02-26 11:56:25 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,4 +8,10 @@ def countdown(count)
 end
 
 def countdown_with_sleep(count)
-  
+  while seconds > 0
+    puts "#{count} SECOND(S)!"
+    seconds -= 1
+    sleep(1)
+  end
+  return "HAPPY NEW YEAR!"
+end
